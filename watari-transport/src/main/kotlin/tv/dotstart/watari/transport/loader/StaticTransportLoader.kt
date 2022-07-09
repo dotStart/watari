e8a49130bee7c3490dfe95e1e59f91f5a7a57ac9
Johannes Donath <dotstart@pm.me>
2022-07-10 01:51:36 +0200

    Created a static transport loader.

---
+++ watari-transport/src/main/kotlin/tv/dotstart/watari/transport/loader/StaticTransportLoader.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.watari.transport.loader
+
+import tv.dotstart.watari.transport.Transport
+
+/**
+ * Provides discovery of a predefined static list of transports.
+ *
+ * @author Johannes Donath
+ * @date 10/07/2022
+ * @since 0.1.0
+ */
+class StaticTransportLoader(transports: List<Transport>) : TransportLoader {
+
+  override val installed = ArrayList(transports)
+
+  override val available = installed
+    .filter(Transport::available)
+
+  override val optimal: Transport? = available
+    .maxBy(Transport::priority)
+}
